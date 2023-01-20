1a494282b152d983d6be77bd5e260a2d4d27a68c
prashant1307 <prashantlorgev8@gmail.com>
2023-01-20 18:27:47 +0530

    added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const PORT = process.env.PORT || 8080;
 const express = require('express')
 
+const cors=require("cors");
+
+
 const app = express()
+app.use(cors())
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
